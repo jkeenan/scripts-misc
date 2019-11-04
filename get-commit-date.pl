2049c02bfde6d3c2f78d71fd55163a0186d93f6a
James E Keenan <jkeenan@cpan.org>
2019-11-03 21:51:10 -0500

    After bisecting for first bad commit, get commit date.
    We use Porting/bisect.pl -- a wrapper around 'git bisect' -- to identify
    problematic commits within the Perl 5 core distribution.  This program
    can be used when our objective is to get the "first bad commit" -- i.e.,
    the SHA of the commit which "broke" the code.  The program will output
    the first five lines of the output from 'git log --format=fuller' for
    the "bad" commit, thereby displaying the committer and the commit date.
    ('git bisect' only displays the author and the patch date, but the
    committer and commit date are often useful as well.)

---
+++ get-commit-date.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+use 5.14.0;
+use warnings;
+use Carp;
+
+# Provided that you are bisecting for the purpose of finding the "first bad
+# commit", run this program from the directory in which you have just run
+# Porting/bisect.pl.
+
+# Try to identify date when bad commit was committed to blead
+# (as distinct from date patch was written).
+
+my $HEAD = '';
+my $default_log = "./.git/BISECT_LOG"; 
+my $blog = (-f $default_log) ? $default_log : shift(@ARGV);
+open my $IN, '<', $blog or die "Unable to open $blog for reading";
+while (my $l = <$IN>) {
+    chomp $l;
+    next unless $l =~ m{^#\sfirst\sbad\scommit:\s\[([^\]]+)\]};
+    $HEAD = $1;
+    last;
+}
+my $message = <<EOM;
+Unable to extract HEAD position from $blog.
+This may be either because the objective in this bisection
+was something other than 'find the first bad commit', or
+because the program could not parse $blog to extract the
+SHA for the first bad commit.
+EOM
+
+croak($message) unless length $HEAD;
+
+my @git_log_output = `git log --format=fuller $HEAD`;
+chomp(@git_log_output);
+unless (@git_log_output >= 5) {
+    croak "Unable to capture output of 'git log --format=fuller'";
+}
+else {
+    say $_ for @git_log_output[0..4];
+}
+
